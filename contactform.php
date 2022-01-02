5a8aa8ff301397d2454501c343358f2a7388939a
Vortechs299 <55398443+Vortechs299@users.noreply.github.com>
2022-01-02 18:05:24 +0100

    mailforms

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject']
+    $mailfrom = $_POST['email']
+    $message = $_POST['message']
+    $telephone = $_POST['telephone']
+
+    $mailTo = "antonobroos@gmail.com";
+    $headers = "van: ".$mailfrom;
+    $txt = "Dit bericht is verstuurd vanuit kblsbv.be.\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
+}
+?>
