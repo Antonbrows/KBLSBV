94d361539614a760ee9e420688d13ceb84c0ddbc
Vortechs299 <55398443+Vortechs299@users.noreply.github.com>
2022-01-02 19:56:22 +0100

    sendmails5

--- contactform.php
+++ contactform.php
@@ -1,10 +1,10 @@
 <?php
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
-    $subject = $_POST['subject']
-    $mailfrom = $_POST['email']
-    $message = $_POST['message']
-    $telephone = $_POST['telephone']
+    $subject = $_POST['subject'];
+    $mailfrom = $_POST['email'];
+    $message = $_POST['message'];
+    $telephone = $_POST['telephone'];
 
     $mailTo = "karelbroos@kblsbv.be";
     $headers = "From: ".$mailfrom;
